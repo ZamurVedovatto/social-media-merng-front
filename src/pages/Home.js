599b37b76563177b671a2e1b378bde870dd70c70
Zamur Vedovatto <vedovatto@gmail.com>
2021-03-14 19:45:44 -0300

    adding posts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from "@apollo/client"
-import gql from 'graphql-tag'
 import { Grid } from 'semantic-ui-react'
+
 import PostCard from './../components/PostCard'
+import PostForm from './../components/PostForm'
+
+import { FETCH_POSTS_QUERY } from './../util/graphql'
+
+import { AuthContext } from './../context/auth'
 
 export default function Home() {
+  const { user } = useContext(AuthContext)
   const { loading, data } = useQuery(FETCH_POSTS_QUERY)
 
   return (
@@ -13,6 +19,11 @@ export default function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts...</h1>
         ) : (
@@ -27,25 +38,3 @@ export default function Home() {
     </Grid>
   )
 }
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`
